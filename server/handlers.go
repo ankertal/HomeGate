7d3afe51a20ef889d69a9f0f155ff63b091f1b22
Yaron Weinsberg <wyaron@gmail.com>
2021-05-20 16:41:15 +0000

    times endpoint

--- server/handlers.go
+++ server/handlers.go
@@ -7,6 +7,7 @@ import (
 	"net/http/httputil"
 	"time"
 
+	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -46,7 +47,25 @@ func (srv *HomeGateServer) home(w http.ResponseWriter, r *http.Request) {
 
 	dumpRequest(w, r)
 
-	fmt.Fprintf(w, "HomeGate Server @ %v, version: %v", time.Now(), version)
+	fmt.Fprintf(w, "HomeGate Server @ %v, version: %v\n", time.Now(), version)
+}
+
+func (srv *HomeGateServer) times(w http.ResponseWriter, r *http.Request) {
+	srv.Lock()
+	defer srv.Unlock()
+
+	dumpRequest(w, r)
+
+	params := mux.Vars(r)
+	deploymentName := params["deployment"]
+	dep, ok := srv.deployments[deploymentName]
+	if ok {
+		fmt.Fprintf(w, "Last Open for Deployment : %v --> %v\n", deploymentName, dep.lastOpen)
+		fmt.Fprintf(w, "Last Close for Deployment: %v --> %v\n", deploymentName, dep.lastClose)
+		fmt.Fprintf(w, "Last Got Command for RC  : %v --> %v\n", deploymentName, dep.lastGotCommand)
+	} else {
+		fmt.Fprintf(w, "Could not find a deployment: %v", deploymentName)
+	}
 }
 
 func (srv *HomeGateServer) checkGateRequestParams(w http.ResponseWriter, deploymentName, userName, password *string) (*deployment, error) {
@@ -76,9 +95,6 @@ func (srv *HomeGateServer) checkGateRequestParams(w http.ResponseWriter, deploym
 }
 
 func (srv *HomeGateServer) open(w http.ResponseWriter, r *http.Request) {
-	srv.Lock()
-	defer srv.Unlock()
-
 	dumpRequest(w, r)
 
 	w.Header().Set("Content-Type", "application/json")
@@ -90,19 +106,20 @@ func (srv *HomeGateServer) open(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	srv.Lock()
+	defer srv.Unlock()
+
 	deployment, err := srv.checkGateRequestParams(w, openEvent.Deployment, openEvent.User, openEvent.Password)
 	if err != nil {
 		log.Printf("%v", err)
 		return
 	}
 	deployment.rcState = Open
+	deployment.lastOpen = time.Now()
 
 }
 
 func (srv *HomeGateServer) close(w http.ResponseWriter, r *http.Request) {
-	srv.Lock()
-	defer srv.Unlock()
-
 	dumpRequest(w, r)
 
 	w.Header().Set("Content-Type", "application/json")
@@ -114,19 +131,21 @@ func (srv *HomeGateServer) close(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	srv.Lock()
+	defer srv.Unlock()
+
 	deployment, err := srv.checkGateRequestParams(w, closeEvent.Deployment, closeEvent.User, closeEvent.Password)
 	if err != nil {
 		log.Printf("%v", err)
 		return
 	}
+
 	deployment.rcState = Close
+	deployment.lastClose = time.Now()
 
 }
 
 func (srv *HomeGateServer) rcStatus(w http.ResponseWriter, r *http.Request) {
-	srv.Lock()
-	defer srv.Unlock()
-
 	dumpRequest(w, r)
 
 	w.Header().Set("Content-Type", "application/json")
@@ -138,6 +157,9 @@ func (srv *HomeGateServer) rcStatus(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	srv.Lock()
+	defer srv.Unlock()
+
 	deployment, err := srv.checkGateRequestParams(w, statusEvent.Deployment, statusEvent.User, statusEvent.Password)
 	if err != nil {
 		log.Printf("%v", err)
@@ -145,6 +167,10 @@ func (srv *HomeGateServer) rcStatus(w http.ResponseWriter, r *http.Request) {
 	}
 
 	respondWithJSON(w, http.StatusOK, map[string]string{"status": fmt.Sprintf("%v", deployment.rcState)})
+
+	if deployment.rcState == Open || deployment.rcState == Close || deployment.rcState == Update {
+		deployment.lastGotCommand = time.Now()
+	}
 
 	deployment.rcState = Unknown
 }
