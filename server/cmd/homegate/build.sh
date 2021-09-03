2c29ca01299b9ba887d3238b5a93b38d166a610d
yaron weinsberg <wyaron@gmail.com>
2021-09-03 11:52:42 +0000

    build script

--- server/cmd/homegate/build.sh
+++ server/cmd/homegate/build.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+echo "stop homegate service"
+
+sudo systemctl stop homegate
+rm ./homegate
+
+echo "clear homegate service journal"
 sudo journalctl --rotate
 sudo journalctl --vacuum-time=1s
 
+echo "building..."
 go build ./...
+
+
+echo "starting homegate service"
+sudo systemctl start homegate
+
 
 
